9ed511eff4a9ea97c09967da99cc8b8ac009c000
João Henrique da Silva <joaohenriquedasilva244@gmail.com>
2026-05-27 10:11:46 -0300

    feat: implementa navegacao Jetpack Compose entre a tela de login e a de cadastro

--- app/src/main/java/com/example/lotoeasy/MainActivity.kt
+++ app/src/main/java/com/example/lotoeasy/MainActivity.kt
@@ -3,27 +3,61 @@ package com.example.lotoeasy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.lotoeasy.ui.screens.LoginScreen
+import com.example.lotoeasy.ui.screens.RegisterScreen
 import com.example.lotoeasy.ui.theme.LotoeasyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             LotoeasyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    LoginScreen(
-                        modifier = Modifier.padding(innerPadding),
-                        onLoginClick = {
-                            /* Lógica de navegação  */
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+
+                    //Rotas do app
+                    NavHost(
+                        navController = navController,
+                        startDestination = "login"
+                    ) {
+                        // Rota da Tela de Login
+                        composable("login") {
+                            LoginScreen(
+                                onLoginClick = {
+                                },
+                                onRegisterClick = {
+                                    navController.navigate("register")
+                                }
+
+                            )
                         }
-                    )
+
+                        // Rota da Tela de Cadastro
+                        composable("register") {
+                            RegisterScreen(
+                                onRegisterClick = {
+                                    navController.navigate("login") {
+                                        popUpTo("login") { inclusive = true }
+                                    }
+                                },
+                                onBackToLoginClick = {
+                                    navController.navigate("login") {
+                                        popUpTo("login") { inclusive = true }
+                                    }
+                                }
+                            )
+                        }
+                    }
                 }
             }
         }
